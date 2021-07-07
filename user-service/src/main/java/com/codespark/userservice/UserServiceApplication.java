28f59f6bb25215beef912f6c3fbf4f6f22fe9fb6
Subhankar Das <subhankardas831@gmail.com>
2021-07-07 16:59:29 +0530

    Added gateway and routing using Spring Cloud Gateway

--- user-service/src/main/java/com/codespark/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/codespark/userservice/UserServiceApplication.java
@@ -2,12 +2,26 @@ package com.codespark.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping
+class UserController {
+
+	@GetMapping("/us/msg")
+	public String message() {
+		return "Test Message!";
 	}
 
 }
