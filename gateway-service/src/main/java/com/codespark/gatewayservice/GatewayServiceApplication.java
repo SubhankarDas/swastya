4eb74f90445992816c0030db18fc41ec532ac0e9
Subhankar Das <subhankardas831@gmail.com>
2021-07-13 19:16:53 +0530

    Modified project to use multi module structure

--- gateway-service/src/main/java/com/codespark/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/codespark/gatewayservice/GatewayServiceApplication.java
@@ -3,6 +3,16 @@ package com.codespark.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Gateway Service for API Routing
+ * 
+ * Spring Cloud Gateway aims to provide a simple, yet effective way to route to
+ * APIs and provide cross cutting concerns to them such as: security,
+ * monitoring/metrics, and resiliency.
+ * 
+ * @author Subhankar Das
+ *
+ */
 @SpringBootApplication
 public class GatewayServiceApplication {
 
