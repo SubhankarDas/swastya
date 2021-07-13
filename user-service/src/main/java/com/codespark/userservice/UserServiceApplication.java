018bad78d2efbe474282c7ce459a2d7397628c2c
Subhankar Das <subhankardas831@gmail.com>
2021-07-13 10:21:16 +0530

    Added config service encryption for properties

--- user-service/src/main/java/com/codespark/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/codespark/userservice/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.codespark.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +20,25 @@ public class UserServiceApplication {
 @RequestMapping
 class UserController {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@GetMapping("/v1/users")
 	public String usersv1() {
+		User user = new User();
+		user.setName("TEST USER 1");
+
+		userRepository.save(user);
 		return "users v1";
 	}
 
 	@GetMapping("/v2/users")
 	public String usersv2() {
+		User user = new User();
+		user.setName("TEST USER 2");
+
+		userRepository.save(user);
 		return "users v2";
 	}
-	
+
 }
